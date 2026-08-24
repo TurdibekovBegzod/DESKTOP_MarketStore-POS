da8fe8a36a419ce9f19f6585b049b429aeea148a
Begzod <begzodasidev@gmail.com>
2026-08-24 13:18:53 +0300

    Release v1.0.2: Production build with dynamic tags and AppData persistence

--- app/version.py
+++ app/version.py
@@ -3,7 +3,7 @@ MarketStore POS Application Version and Metadata
 """
 
 APP_NAME = "MarketStore POS"
-APP_VERSION = "1.0.1"
-APP_BUILD = 1
+APP_VERSION = "1.0.2"
+APP_BUILD = 2
 APP_RELEASE_DATE = "2026-08-24"
 APP_AUTHOR = "MarketStore Team"
