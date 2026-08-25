c6a8e3ec3de87782e082c9dba81dbe0362e0fa50
Begzod <begzodasidev@gmail.com>
2026-08-26 00:21:15 +0300

    chore(release): bump version to 1.1.0

--- app/version.py
+++ app/version.py
@@ -3,7 +3,7 @@ MarketStore POS Application Version and Metadata
 """
 
 APP_NAME = "MarketStore POS"
-APP_VERSION = "1.0.4"
-APP_BUILD = 4
+APP_VERSION = "1.1.0"
+APP_BUILD = 5
 APP_RELEASE_DATE = "2026-08-25"
 APP_AUTHOR = "MarketStore Team"
